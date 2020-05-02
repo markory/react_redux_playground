f32623d86d38040d45df411734ada6d99b3b500d
Philipp Müller-Leven <p.muellerleven@gmail.com>
2020-05-02 16:50:55 +0200

    simple project start

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react_redux_playground
+
 Playground for experience some functionality of react and redux.
+
+## Link complete code
+
+https://github.com/StephenGrider/redux-code
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    const buttonText = 'Click me!';
+    const style = { backgroundColor: 'blue', color: 'white' };
+
+    return (
+        <div>
+            <label className="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={style}>{buttonText}</button>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
