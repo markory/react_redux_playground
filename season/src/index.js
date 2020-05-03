649e45ad24e809629e910339d935c1d3d9b466f5
Philipp Müller-Leven <p.muellerleven@gmail.com>
2020-05-03 18:07:54 +0200

    use lifecylce methode for locator

--- season/src/index.js
+++ season/src/index.js
@@ -2,13 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    
-    render() {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+    }
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
-        return <div>Latitude:</div>;
+    }
+
+    componentDidUpdate() {
+        console.log('My component was updated');
+    }
+
+    // React says we have to define render!!
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading!</div>;
     }
 }
 
