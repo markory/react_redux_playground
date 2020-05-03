82813e2487ba3c6202f1eb441787ddd22d2c1712
Philipp Müller-Leven <p.muellerleven@gmail.com>
2020-05-03 18:50:53 +0200

    Add some styling for summer/winter display

--- season/src/index.js
+++ season/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
     constructor(props) {
@@ -25,7 +26,7 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
         return <div>Loading!</div>;
