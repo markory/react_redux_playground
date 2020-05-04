974de1e85aa9f0c31b8197b98eb31124eec56d8c
Philipp Müller-Leven <p.muellerleven@gmail.com>
2020-05-04 23:00:02 +0200

    Pass prop to parent by callback

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onInputChange = (event) => {
+        console.log(event.target.value);
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text"></input>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(e) =>
+                                this.setState({ term: e.target.value })
+                            }
+                        ></input>
                     </div>
                 </form>
             </div>
